24b01b95c326a2fd6bb8aaf4deb04c0c8d027866
ArkMan13 <74531609+ArkMan13@users.noreply.github.com>
2021-01-15 17:22:56 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,44 @@
+var bullet, wall;
+
+var speed, weight, thickness;
+
+function setup() {
+  createCanvas(1600,400);
+  speed = random(55,90);
+  weight = random(400,1500);
+  thickness = random(22,83);
+  bullet = createSprite(50, 200, 50, 5);
+  wall = createSprite(1200, 200, thickness, height/2);
+  bullet.velocityX = speed;
+  bullet.shapeColor = color(255);
+  wall.shapeColor = color(80);
+}
+
+function draw() {
+  background("black");  
+
+  if(hasCollided(bullet, wall)) {
+    bullet.velocityX=0;
+    var deformation=0.5*weight*speed*speed/(thickness*thickness*thickness);
+    if(deformation>10) {
+      wall.shapeColor=color(255,0,0);
+    }
+    
+    if(deformation<10) {
+      wall.shapeColor=color(0,255,0);
+    }
+  }
+  console.log(deformation);
+
+  drawSprites();
+
+}
+
+function hasCollided( lbullet, lwall) {
+  bulletRightEdge=lbullet.x+lbullet.width;
+  wallLeftEdge=lwall.x;
+  if(bulletRightEdge>=wallLeftEdge){
+    return true;
+  }
+  return false;
+}
